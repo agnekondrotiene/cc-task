2efae888b752eed4265ca432866866a3ffdbd37c
Agne Kondrotiene <ag.kondrotiene@gmail.com>
2022-01-08 15:29:08 +0200

    handle expanded state

--- src/components/checkout/variants/CheckoutVariants.tsx
+++ src/components/checkout/variants/CheckoutVariants.tsx
@@ -1,16 +1,25 @@
-import React from "react";
+import { useState } from "react";
 import { VariantsHeader } from "./VariantsHeader";
 import { VariantsList } from "./VariantsList";
 
 export const CheckoutVariants = () => {
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const handleExpandClick = () => {
+    setIsExpanded((current) => !current);
+  };
+
   return (
     <div>
       <VariantsHeader
         image="assets/images/leggings.png"
         name="CoreProduct"
         quantity={1}
+        isExpanded={isExpanded}
+        onClick={handleExpandClick}
       />
-      {/* <VariantsList /> */}
+
+      {isExpanded && <VariantsList />}
     </div>
   );
 };
--- src/components/checkout/variants/VariantsHeader.tsx
+++ src/components/checkout/variants/VariantsHeader.tsx
@@ -1,15 +1,22 @@
 import { ProductInfo, ProductInfoProps } from "../ProductInfo";
 import { ReactComponent as ArrowDown } from "assets/arrow-down.svg";
 
-type Props = ProductInfoProps;
+type Props = ProductInfoProps & {
+  isExpanded: boolean;
+  onClick: () => void;
+};
 
 export const VariantsHeader = (props: Props) => {
+  const { isExpanded, onClick, ...productInfoProps } = props;
   return (
     <div className="flex justify-between items-center">
-      <ProductInfo {...props} />
-      <span className="flex items-center gap-x-[10px] text-small2 text-M1 font-black">
+      <ProductInfo {...productInfoProps} />
+      <span
+        className="flex items-center gap-x-[10px] text-small2 text-M1 font-black"
+        onClick={onClick}
+      >
         Change
-        <span className={props.isActive ? "rotate-180" : undefined}>
+        <span className={isExpanded ? "rotate-180" : undefined}>
           <ArrowDown />
         </span>
       </span>
