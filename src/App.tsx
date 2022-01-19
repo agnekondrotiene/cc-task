c17d4fd08047139b13c1df03b7043f5cc092a108
Agne Kondrotiene <ag.kondrotiene@gmail.com>
2022-01-19 16:23:18 +0200

    add Footer Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Footer } from "components/Footer";
 import { CheckoutPage } from "pages/CheckoutPage";
 
 function App() {
   return (
     <div className="bg-B1">
       <CheckoutPage />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { ReactComponent as Logo } from "assets/logoipsum.svg";
 
 export const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="flex justify-between items-center bg-M1 py-[20px] pl-[170px] pr-[75px] font-sofiapro text-White text-small1 font-normal leading-[15px]">
+      <div className="flex justify-between items-center gap-x-[32px] ">
+        <Logo />
+        <h2>Terms of Service</h2>
+        <h2>Privacy Policy</h2>
+        <h2>Returns</h2>
+        <h2>FAQ</h2>
+      </div>
+      <div>
+        <h2>© 2022 Logoipsum. All rights reserved</h2>
+      </div>
+    </div>
+  );
 };
